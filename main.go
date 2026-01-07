a426d67398ed502591983976c50a1ff6fd4cd72a
Michael Papapetros <michalispap99@github.com>
2026-01-07 19:43:23 +0200

    Add table

--- main.go
+++ main.go
@@ -8,10 +8,33 @@ import (
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+var (
+	higherStyle  = lipgloss.NewStyle().Foreground(lipgloss.Color("10"))
+	lowerStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("9"))
+	correctStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("12")).Bold(true)
+)
+
+var (
+	purple    = lipgloss.Color("99")
+	gray      = lipgloss.Color("245")
+	lightGray = lipgloss.Color("241")
+
+	headerStyle  = lipgloss.NewStyle().Foreground(purple).Bold(true).Align(lipgloss.Center)
+	cellStyle    = lipgloss.NewStyle().Padding(0, 1).Width(14)
+	oddRowStyle  = cellStyle.Foreground(gray)
+	evenRowStyle = cellStyle.Foreground(lightGray)
 )
 
 func main() {
-	p := tea.NewProgram(initialModel())
+	p := tea.NewProgram(
+		initialModel(),
+		tea.WithAltScreen(),
+	)
+
 	if _, err := p.Run(); err != nil {
 		panic(err)
 	}
@@ -25,6 +48,26 @@ func generateRandomNumber() int {
 	return randomNumber
 }
 
+func createTable(rows [][]string) string {
+	t := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(purple)).
+		StyleFunc(func(row, col int) lipgloss.Style {
+			switch {
+			case row == table.HeaderRow:
+				return headerStyle
+			case row%2 == 0:
+				return evenRowStyle
+			default:
+				return oddRowStyle
+			}
+		}).
+		Headers("Attempt", "Your Guess", "Hint/Result").
+		Rows(rows...)
+
+	return t.String()
+}
+
 type (
 	errMsg error
 )
@@ -34,6 +77,7 @@ type model struct {
 	secret    int
 	attempts  int
 	feedback  string
+	history   [][]string
 	err       error
 }
 
@@ -49,6 +93,7 @@ func initialModel() model {
 		secret:    generateRandomNumber(),
 		attempts:  0,
 		feedback:  "",
+		history:   [][]string{},
 		err:       nil,
 	}
 }
@@ -67,16 +112,19 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			userInput, err := strconv.Atoi(m.textInput.Value())
 			m.textInput.SetValue("")
 			if err != nil {
-				m.feedback = "Please enter a number"
+				m.feedback = "Enter a number, dude!"
 				return m, nil
 			}
 			m.attempts++
 			if userInput < m.secret {
-				m.feedback = "higher"
+				m.feedback = "Higher"
+				m.history = append(m.history, []string{strconv.Itoa(m.attempts), strconv.Itoa(userInput), "⬆️"})
 			} else if userInput > m.secret {
-				m.feedback = "lower"
+				m.feedback = "Lower"
+				m.history = append(m.history, []string{strconv.Itoa(m.attempts), strconv.Itoa(userInput), "⬇️"})
 			} else {
-				m.feedback = "correct"
+				m.feedback = "Correct!"
+				m.history = append(m.history, []string{strconv.Itoa(m.attempts), strconv.Itoa(userInput), "✅"})
 				return m, tea.Quit
 			}
 
@@ -94,10 +142,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
+	styledFeedback := m.feedback
+
+	switch m.feedback {
+	case "Higher":
+		styledFeedback = higherStyle.Render(m.feedback)
+	case "Lower":
+		styledFeedback = lowerStyle.Render(m.feedback)
+	case "Correct!":
+		styledFeedback = correctStyle.Render(m.feedback)
+	}
+
 	return fmt.Sprintf(
-		"Guess the number (1-100)\n\n%s\n\n%s\nattempts: %d\n\n(enter to submit, esc to quit)\n",
+		"Welcome to the Guessing Game!\n\nGuess the number between 1 and 100\n\n%s\n\n%s\nAttempts so far: %d\n\n%s\n\n(enter to submit, esc to quit)\n",
 		m.textInput.View(),
-		m.feedback,
+		styledFeedback,
 		m.attempts,
+		createTable(m.history),
 	)
 }
